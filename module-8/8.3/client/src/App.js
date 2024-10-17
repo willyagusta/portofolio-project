f049ee9da8bc75cf7c83a6d3da4019af97b1478f
Willy Agusta <agustawilly@gmail.com>
2024-10-17 20:04:16 +0700

    adding 404 error page and contact form

--- module-8/8.3/client/src/App.js
+++ module-8/8.3/client/src/App.js
@@ -6,6 +6,7 @@ import Blogs from './pages/Blogs';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
